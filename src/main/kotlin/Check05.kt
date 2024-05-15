448a6b883c7b4e73c05827cbab2267d620553ca5
s23022 <s23022@std.it-college.ac.jp>
2024-05-15 10:04:03 +0900

    十三章〜十五章 終わり 5月14日 Check3~Check05

--- src/main/kotlin/Check05.kt
+++ src/main/kotlin/Check05.kt
@@ -1,5 +1,46 @@
 package org.example
 
-fun main() {
+import kotlin.random.Random
+import kotlin.random.nextInt
 
+fun main() {
+// Check03 で作ったプログラムが、本当に確率通りか試してみる
+    print("試行回数?: ")
+    val count = readln().toInt()
+
+    // かち・まけ・あいこ の数を数える変数を用意する
+    var win = 0
+    var lose = 0
+    var draw = 0
+
+    for (i in 0..<count) {
+        val player = Random.nextInt(1..3)
+        println("1:グー 2:チョキ 3:パー を選びました: $player")
+
+        // 乱数で勝敗を決める
+        val (result, judge) = when (Random.nextInt(0..<100)) {
+            in 0..<10 -> {
+                win++
+                Pair(1, "かち")
+            }
+            in 10..<15 -> {
+                draw++
+                Pair(0, "あいこ")
+            }
+            else -> {
+                lose++
+                Pair(2, "まけ")
+            }
+        }
+        // CPUの手を計算で求める
+        val cpu = (player + result) % 3
+
+        // 結果表示
+        // 勝敗の結果表示(プレイヤー視点)
+        println("CPU: ${cpu + 1} --- $judge")
+    }
+    println("確率の結果")
+    println("かち: $win (${win / count.toDouble() * 100}%)")
+    println("まけ: $lose (${lose / count.toDouble() * 100}%)")
+    println("あいこ: $draw (${draw / count.toDouble() * 100}%)")
 }
