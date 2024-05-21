067f1533aa56b9c3e6142c33f03290bf6c1c7fee
s23022 <s23022@std.it-college.ac.jp>
2024-05-21 10:16:16 +0900

    練習用

--- src/main/kotlin/for practice06.kt
+++ src/main/kotlin/for practice06.kt
@@ -1,2 +1,16 @@
 package org.example
 
+fun main(args: Array<String>) {
+    val p = Sample()
+    p.name = "たけし"
+    p.age = 5
+    p.greet()
+}
+class Sample() {
+    var name: String = ""
+    var age: Int = 0
+    fun greet() {
+        println(" ${name}です。${age}歳です")
+    }
+}
+
---
+++ src/main/kotlin/for practice66.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main(args: Array<String>) {
+    val a = Result("ゆうた", 5)
+    a.result1()
+}
+class Result  constructor(var name: String = "", var age: Int = 0){
+    fun result1() {
+        println(" ${name}です ${age}です")
+    }
+
+
+}
